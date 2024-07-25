9b04355ac21c83399ea27dbf7f1608275229fc31
Francisco Santana <franciscosantana203@gmail.com>
2024-07-25 19:55:46 -0300

    feat(*):main pages

---
+++ src/app/games/layout.tsx
@@ -0,0 +1,12 @@
+export default function GamesLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <p>Games layout</p>
+      {children}
+    </div>
+  );
+}
